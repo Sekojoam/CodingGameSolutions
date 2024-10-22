bfbaa8d93094c74a980acdbf6a44f3eb50bd391b
Sekojoam <sekojoam@gmail.com>
2024-10-22 09:25:10 +0200

    six degrees of kevin bacon | solution

--- puzzles/six_degrees_of_kevin_bacon/main.py
+++ puzzles/six_degrees_of_kevin_bacon/main.py
@@ -5,75 +5,82 @@ import math
 def debug(message, params=""):
     print(f"{params}:{message}", file=sys.stderr, flush=True)
 
+
 # 6 Degrees of Kevin Bacon!
+
 
 class Node:
 
-    def __init__(self, isFirst, linkedActors, name, movieName, previousNode:list):
-        self.isFirst = isFirst
-        self.linkedActors = linkedActors
+    def __init__(self, name):
         self.name = name
-        self.movieName = movieName
-        self.previousNode = previousNode
+        self.parentsList = []
 
-    def __str__(self):
-        return f"name : {self.name}, isFirst {self.isFirst}, actors {[i.name for i in self.linkedActors]} previousNode {self.previousNode}"
 
-class Movie:
+class Graph:
 
-    def __init__(self, title, actors):
-        self.title = title
+    def __init__(self, movies, actors):
+        self.queue: list[Node] = []
+        self.movies = movies
         self.actors = actors
+
+    def bfs(self, actorName):
+        kevin = "Kevin Bacon"
+        if actor_name == kevin:
+            print("0")
+            return
+        kevinFound = False
+        node = Node(actorName)
+        self.queue.append(node)
+        movies = self.actors[node.name]
+        while not kevinFound:
+            for node in self.queue:
+                movies = self.actors[node.name]
+                for movie in movies:
+                    movie_actors = self.movies[movie]
+                    if kevin in movie_actors:
+                        kevinFound = True
+                        debug(movie, "kevin found in ")
+                        debug(node.parentsList, "kevin found in ")
+                        print(len(node.parentsList) + 1)
+                        break
+                    else:
+                        if node.name in movie_actors:
+                            movie_actors.remove(node.name)
+                        for movie_actor in movie_actors:
+                            newNode = Node(movie_actor)
+                            debug(node.name, "node.name")
+                            debug(node.parentsList, "node.parentsList")
+                            newNode.parentsList = list(node.parentsList)
+                            if node.name not in newNode.parentsList:
+                                newNode.parentsList.append(node.name)
+                            self.queue.append(newNode)
+                if kevinFound:
+                    break
+
 
 actor_name = input()
 debug(actor_name, "actor_name")
+movies = {}
+actors = {}
 n = int(input())
-movie_list = []
-node = None
-
-specialName = "Kevin Bacon"
-solutionFound = False
-finalNode = None
 for i in range(n):
     movie_cast = input()
-    title, actors = movie_cast.split(": ")
-    actors = actors.split(", ")
-    if actor_name in actors:
-        node = Node(isFirst=True, linkedActors = [], name = actor_name, movieName = title, previousNode= [])
-        for linkedActor in actors:
-            if linkedActor == actor_name:
-                continue
-            node.linkedActors.append(Node(isFirst=False, linkedActors = [], name = linkedActor, movieName=None, previousNode=[title]))
+    movie_title, movie_actors = movie_cast.split(": ")
+    movie_actors = movie_actors.split(", ")
+    movies[movie_title] = movie_actors
+    for actor in movie_actors:
+        if actor in actors:
+            actors[actor].append(movie_title)
+        else:
+            actors[actor] = [movie_title]
 
-            if linkedActor == specialName:
-                solutionFound = True
-                print("1")
-        debug(node, "node")
-    debug(title, "title")
-    debug(actors, "actors")
-    movie_list.append(Movie(title, actors))
-if node is not None:
-    for actor in node.linkedActors:
-        for movie in movie_list:
-            # don't forget 
-            if movie.title in actor.previousNode:
-                continue
-            if actor.name in movie.actors:
-                actor.movieName = movie.title
-                for linkedActor in movie.actors:
-                    if linkedActor == actor.name:
-                        continue
-                    previousNode = list(actor.previousNode)
-                    previousNode.append(movie.title)
-                    actor.linkedActors.append(Node(isFirst=False, linkedActors = [], name = linkedActor, movieName=None, previousNode=previousNode))
-                    if linkedActor == specialName:
-                        solutionFound = True
-                        print(len(previousNode))
-                # new_node = Node(isFirst=False, linkedActors = [], name = actor_name, movieName = title, previousNode=None)
+debug(movies, "movies")
+debug(actors, "actors")
 
+graph = Graph(movies, actors)
 
-
+graph.bfs(actor_name)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-# print(solutionFound)
+# print("N degrees to Kevin Bacon")
